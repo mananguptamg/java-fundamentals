99305bfe654b6262a4082731e53ae06cb4870813
Manan Gupta <101213247+mananguptamg@users.noreply.github.com>
2025-02-27 12:55:30 +0530

    [Manan] ADD: Finding area of circle

--- AreaofCircle.java
+++ AreaofCircle.java
@@ -1,16 +1,24 @@
+// Importing the Scanner class to take user input
 import java.util.Scanner;
 
-class AreaofCircle{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		
-		double radius;
-		double areaofCircle;
-		
-		System.out.println("Enter the radius of circle");
-		radius = sc.nextDouble();
+class AreaofCircle {
+    public static void main(String[] args) {
+        
+        // Creating a Scanner object to read input from the user
+        Scanner sc = new Scanner(System.in);
+        
+        // Declaring variables for radius and area of the circle
+        double radius;
+        double areaofCircle;
+        
+        // Prompting the user to enter the radius of the circle
+        System.out.println("Enter the radius of the circle:");
+        radius = sc.nextDouble(); // Reading radius input from user
 
-		areaofCircle = 22/7 * radius * radius;
-		System.out.print("Area of circle with radius " +radius+ " is " +areaofCircle);
-	}
+        // Calculating the area of the circle using the formula: π * r^2
+        areaofCircle = Math.PI * radius * radius; 
+        
+        // Displaying the calculated area
+        System.out.println("Area of the circle with radius " + radius + " is " + areaofCircle);
+    }
 }
