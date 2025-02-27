c69db02ac34fc1ba46426db57f2238548a40a0b4
Manan Gupta <101213247+mananguptamg@users.noreply.github.com>
2025-02-27 12:54:10 +0530

    [Manan] ADD: Converting Celsius to Fahrenheit

--- CelsiusToFahrenheitConversion.java
+++ CelsiusToFahrenheitConversion.java
@@ -1,15 +1,23 @@
+// Importing the Scanner class to take user input
 import java.util.Scanner;
 
-class CelsiusToFahrenheitConversion{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		
-		double celsiusTemperature;
-		double fahrenheitTemperature;
+class CelsiusToFahrenheitConversion {
+    public static void main(String[] args) {
+        // Creating a Scanner object to read input from the user
+        Scanner sc = new Scanner(System.in);
+        
+        // Declaring variables to store temperatures in Celsius and Fahrenheit
+        double celsiusTemperature;
+        double fahrenheitTemperature;
 
-		System.out.println("Enter temperature in celsius");
-		celsiusTemperature = sc.nextDouble();
-		fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
-		System.out.println("Temperature in fahrenheit is " + fahrenheitTemperature);
-	}
+        // Prompting the user to enter the temperature in Celsius
+        System.out.println("Enter temperature in Celsius:");
+        celsiusTemperature = sc.nextDouble(); // Reading temperature input from user
+        
+        // Converting Celsius to Fahrenheit using the formula: (C × 9/5) + 32
+        fahrenheitTemperature = (celsiusTemperature * 9 / 5) + 32;
+        
+        // Displaying the converted temperature in Fahrenheit
+        System.out.println("Temperature in Fahrenheit is: " + fahrenheitTemperature);
+    }
 }
