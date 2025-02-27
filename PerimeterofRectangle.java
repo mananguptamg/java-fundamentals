18ecc43285bb934377aadf2065fbce17ddba3615
Manan Gupta <101213247+mananguptamg@users.noreply.github.com>
2025-02-27 12:59:46 +0530

    [Manan] ADD: Calculating perimeter of Rectangle

--- PerimeterofRectangle.java
+++ PerimeterofRectangle.java
@@ -1,17 +1,24 @@
+// Importing the Scanner class to take user input
 import java.util.Scanner;
 
 class PerimeterofRectangle {
     public static void main(String[] args) {
+
+        // Creating a Scanner object to read input from the user
         Scanner sc = new Scanner(System.in);
         
+        // Prompting the user to enter the length of the rectangle
         System.out.print("Enter the length of the rectangle: ");
-        double length = sc.nextDouble();
+        double length = sc.nextDouble(); // Reading length input
         
+        // Prompting the user to enter the width of the rectangle
         System.out.print("Enter the width of the rectangle: ");
-        double width = sc.nextDouble();
+        double width = sc.nextDouble(); // Reading width input
         
+        // Calculating the perimeter of the rectangle using the formula: 2 × (length + width)
         double perimeter = 2 * (length + width);
         
-        System.out.println("The perimeter of the rectangle is: " + perimeter);
+        // Displaying the calculated perimeter
+        System.out.printf("The perimeter of the rectangle is: ", perimeter);
     }
 }
