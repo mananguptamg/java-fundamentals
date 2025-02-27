f2aeb5a5eb2e4b81b3e58ba1f91b50137227b98d
Manan Gupta <101213247+mananguptamg@users.noreply.github.com>
2025-02-27 12:53:01 +0530

    [Manan] ADD: Addition of two numbers

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,16 +1,24 @@
 import java.util.Scanner;
 
-class AddTwoNumbers{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		int firstNumber;
-		int secondNumber;
-		
-		System.out.println("Enter first number");
-		firstNumber = sc.nextInt();
-		System.out.println("Enter second number"); 		
-		secondNumber = sc.nextInt();
-		
-		System.out.print("The addition of numbers is " + (firstNumber+secondNumber));
-	}
+class AddTwoNumbers {
+    public static void main(String[] args) {
+        
+        // Creating a Scanner object to read input from the user
+        Scanner sc = new Scanner(System.in);
+        
+        // Declaring two integer variables to store user input
+        int firstNumber;
+        int secondNumber;
+        
+        // Prompting the user to enter the first number
+        System.out.println("Enter first number");
+        firstNumber = sc.nextInt(); // Reading first integer input
+        
+        // Prompting the user to enter the second number
+        System.out.println("Enter second number");  
+        secondNumber = sc.nextInt(); // Reading second integer input
+        
+        // Displaying the sum of the two numbers
+        System.out.print("The addition of numbers is " + (firstNumber + secondNumber));
+    }
 }
